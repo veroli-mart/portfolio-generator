262cc85eac3f624b2f7bdd37be9be0bb7515df96
Veronica Martinez <vmartinez6615@gmail.com>
2020-12-17 16:14:20 -0500

    added new file

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const printProfileData = (profileDataArr) => {
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
-  console.log("================");
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
+// const printProfileData = (profileDataArr) => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
 
-  // Is the same as this...
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem);
-  });
-};
+//   console.log("================");
+
+//   // Is the same as this...
+//   profileDataArr.forEach((profileItem) => {
+//     console.log(profileItem);
+//   });
+// };
+// printProfileData(profileDataArgs);
+fs.writeFile("index.html", generatePage(name, github), (err) => {
+  if (err) throw err;
+
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
+profileDataArgs(generatePage);
