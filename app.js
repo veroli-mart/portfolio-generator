419a3b862f62629e6fc4ac773e01fcbb43cba620
Veronica Martinez <vmartinez6615@gmail.com>
2020-12-17 23:33:30 -0500

    added npm inquirer

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
-// const printProfileData = (profileDataArr) => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
+const inquirer = require("inquirer");
+const iquirer = require("inquirer");
+// const fs = require("fs");
+// const generatePage = require("./src/page-template");
 
-//   console.log("================");
+// const pageHTML = generatePage(name, github);
 
-//   // Is the same as this...
-//   profileDataArr.forEach((profileItem) => {
-//     console.log(profileItem);
-//   });
-// };
-// printProfileData(profileDataArgs);
-fs.writeFile("index.html", generatePage(name, github), (err) => {
-  if (err) throw err;
+// fs.writeFile("./index.html", pageHTML, (err) => {
+//   if (err) throw err;
 
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
